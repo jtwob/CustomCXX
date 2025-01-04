2754dbaf69083ce5fbbded8d3177150c51066012
James Totah <jtotah@usfca.edu>
2025-01-04 13:48:54 -0600

    Basic map tests

--- tests/test_map.cpp
+++ tests/test_map.cpp
@@ -3,12 +3,54 @@
 #include <cassert>
 
 void test_map();
+void test_erase_and_contains();
+void test_basic_operations();
 
 void test_map() {
-    assert(true);
+    test_basic_operations();
+    test_erase_and_contains();
+}
+
+void test_basic_operations() {
+    CustomCXX::Map<int, std::string> map;
+
+    // Insert and access elements
+    map[1] = "one";
+    map[2] = "two";
+
+    assert(map[1] == "one");
+    assert(map[2] == "two");
+
+    // Check size
+    assert(map.size() == 2);
+
+    // Overwrite a value
+    map[1] = "uno";
+    assert(map[1] == "uno");
+
+    std::cout << "Basic operations test passed!" << std::endl;
+}
+
+void test_erase_and_contains() {
+    CustomCXX::Map<int, std::string> map;
+
+    map[1] = "one";
+    map[2] = "two";
+    map[3] = "three";
+
+    // Erase an element
+    map.erase(2);
+    assert(!map.contains(2));
+
+    // Check remaining elements
+    assert(map.contains(1));
+    assert(map.contains(3));
+
+    std::cout << "Erase and contains test passed!" << std::endl;
 }
 
 int main() {
     test_map();
+    std::cout << "All Map tests passed!" << std::endl;
     return 0;
 }
