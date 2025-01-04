decfabf530e135f17df5343a9bd33fa9043620c0
James Totah <jtotah@usfca.edu>
2025-01-04 14:54:02 -0600

    Added tests for erase and contains edge cases, tests pass

--- tests/test_map.cpp
+++ tests/test_map.cpp
@@ -7,11 +7,15 @@ void test_map();
 void test_erase_and_contains();
 void test_basic_operations();
 void test_operator_brackets();
+void test_erase_single_key();
+void test_contains_key_not_present();
 
 void test_map() {
     test_operator_brackets();
     test_basic_operations();
     test_erase_and_contains();
+    test_erase_single_key();
+    test_contains_key_not_present();
 }
 
 void test_basic_operations() {
@@ -57,6 +61,29 @@ void test_operator_brackets() {
     map[1] = "one";
     assert(map[1] == "one");
     std::cout << "Operator[] test passed!" << std::endl;
+}
+
+void test_erase_single_key() {
+    CustomCXX::Map<int, std::string> map;
+
+    map[1] = "one";
+    map[2] = "two";
+
+    assert(map.contains(1));
+    map.erase(1);
+    assert(!map.contains(1)); // Should pass
+    assert(map.contains(2));  // Other keys should remain intact
+
+    std::cout << "Erase single key test passed!" << std::endl;
+}
+
+void test_contains_key_not_present() {
+    CustomCXX::Map<int, std::string> map;
+
+    map[1] = "one";
+
+    assert(!map.contains(2)); // Should pass
+    std::cout << "Contains key not present test passed!" << std::endl;
 }
 
 int main() {
